b16bcfbb7febfbfd36bfd9f3bf9c77d237a62c97
Kiran Murmu <kiranmurmu@outlook.in>
2022-03-17 22:14:22 +0530

    chore: popup page update

--- getting-started/pages/popup.tsx
+++ getting-started/pages/popup.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+
 function Popup() {
-  return <div></div>;
+  const [backgroundColor, setBackgroundColor] = useState<string>("#3aa757");
+
+  const handleButtonClick = async () => {
+    let [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id ?? chrome.tabs.TAB_ID_NONE },
+      files: ["content.js"],
+    });
+  };
+
+  useEffect(() => {
+    chrome.storage.sync.get("color", ({ color }) => {
+      setBackgroundColor(color);
+    });
+  }, [backgroundColor]);
+
+  return (
+    <button
+      id="changeColor"
+      style={{
+        backgroundColor: backgroundColor,
+      }}
+      onClick={handleButtonClick}
+    ></button>
+  );
 }
 
 export default Popup;
