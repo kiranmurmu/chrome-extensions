10984b84f202bee670b0a56961cefaa15d2dc099
Kiran Murmu <kiranmurmu@outlook.in>
2022-03-18 07:15:00 +0530

    chore: options page update

--- getting-started/pages/options.tsx
+++ getting-started/pages/options.tsx
@@ -1,5 +1,48 @@
+import { Fragment, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 function Options() {
-  return <div></div>;
+  const [presetButtonColors] = useState<string[]>([
+    "#3aa757",
+    "#e8453c",
+    "#f9bb2d",
+    "#4688f1",
+  ]);
+  const [currentColor, setCurrentColor] = useState<string>("#3aa757");
+  const handleButtonClick = (color: string) => {
+    setCurrentColor(color);
+  };
+
+  useEffect(() => {
+    if (currentColor) {
+      chrome.storage.sync.set({ color: currentColor });
+    } else {
+      chrome.storage.sync.get("color", ({ color }) => {
+        setCurrentColor(color);
+      });
+    }
+  }, [currentColor]);
+
+  return (
+    <Fragment>
+      <div id="buttonDiv">
+        {presetButtonColors.map((buttonColor) => (
+          <button
+            key={uuidv4()}
+            data-color={buttonColor}
+            style={{
+              backgroundColor: buttonColor,
+            }}
+            className={buttonColor === currentColor ? "current" : ""}
+            onClick={() => handleButtonClick(buttonColor)}
+          ></button>
+        ))}
+      </div>
+      <div>
+        <p>Choose a different background color!</p>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Options;
