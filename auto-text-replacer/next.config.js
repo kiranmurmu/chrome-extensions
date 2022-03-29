e179c866144ef14404e0ff60248c6f2b24de15bb
Kiran Murmu <kiranmurmu@outlook.in>
2022-03-29 10:48:22 +0530

    chore: install & config next-build-id

--- auto-text-replacer/next.config.js
+++ auto-text-replacer/next.config.js
@@ -1,6 +1,9 @@
+const nextBuildId = require("next-build-id");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  generateBuildId: () => nextBuildId({ dir: __dirname }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
